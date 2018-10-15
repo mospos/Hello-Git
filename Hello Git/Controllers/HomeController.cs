921d2c3669468195b83e1b0061b90f15451c0c6c
Moshe Posner <moshepo91@gmail.com>
2018-10-14 23:30:17 -0400

    Added some code

--- Hello Git/Controllers/HomeController.cs
+++ Hello Git/Controllers/HomeController.cs
@@ -10,21 +10,11 @@ namespace Hello_Git.Controllers
     {
         public ActionResult Index()
         {
+            var x = 0;
+            x++;
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        
     }
 }
